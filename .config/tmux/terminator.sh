f96d6d19236ff829f4dcd76604b625e3e7767ec4
Roman Rossamakhin <rossamakhin01@gmail.com>
2026-01-12 10:46:10 +0500

    fix tmux terminator

--- .config/tmux/terminator.sh
+++ .config/tmux/terminator.sh
@@ -2,7 +2,7 @@
 
 export FZF_DEFAULT_OPTS='--bind=alt-k:up,alt-j:down --reverse --style=minimal --tmux 80,10'
 
-LIST=$({
+RESULT=$({
         tmux list-sessions -F '#{?session_attached,❱ #{session_name},}' | sed '/^$/d'
         tmux list-sessions -F '#{?session_attached,,❱ #{session_name}}' | sed '/^$/d'
         find \
@@ -12,11 +12,11 @@ LIST=$({
             $HOME/places/git/work \
             $HOME/places/git/work/launchpad \
             -maxdepth 1 -type d 2> /dev/null
-})
+} | fzf)
 
-RESULT=$(fzf <<<$LIST)
-
-if [[ $RESULT == "/"* ]]; then
+if [[ $RESULT = "" ]]; then
+    exit
+elif [[ $RESULT == "/"* ]]; then
     SEL=$(basename "$RESULT" | tr "[:upper:]:. " "[:lower:]---")
     tmux has-session -t "$SEL" 2>/dev/null || tmux new-session -d -s "$SEL" -c "$RESULT"
 else
