5536e6e4b4e9699db7cd25785122fa64731a3e90
Roman Rossamakhin <rossamakhin01@gmail.com>
2026-03-16 14:37:04 +0500

    try impl light and dark colorscheme

---
+++ .config/nvim/colors/oddsky_light.lua
@@ -0,0 +1,24 @@
+-- stylua: ignore start
+local colors = {
+    red          =  "#cc372e",
+    orange       =  "#e17d19",
+    yellow       =  "#cdac08",
+    green        =  "#26a439",
+    cyan         =  "#089dcb",
+    blue         =  "#0869cb",
+    violet       =  "#9647bf",
+    search       =  "#f5f4d6",
+    visual       =  "#d9e6f2",
+    bg_diff_add  =  "#bcdcbc",
+    bg_diff_del  =  "#ebadad",
+    bg1          =  "#ffffff",
+    bg2          =  "#e6e6e6",
+    bg3          =  "#b3b3b3",
+    bg4          =  "#999999",
+    fg2          =  "#737373",
+    fg1          =  "#1a1a1a",
+}
+-- stylua: ignore end
+
+vim.opt.background = "light"
+require("utils.hls").load(colors)
