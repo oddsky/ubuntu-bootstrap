65ece50ed88a1d2770a83f5f374d7023836cd7ca
Roman Rossamakhin <rossamakhin01@gmail.com>
2026-07-13 12:31:54 +0500

    fix opencode script

--- .local/bin/opencode.sh
+++ .local/bin/opencode.sh
@@ -9,16 +9,22 @@ hF4DGNwPncbTkogSAQdA2JRerCuRCXWvHDMxiGPeFpho8k7pHYjTdigEvz3J90wwvYxoPAZmDSxo\
 7MF5y1d1B/fPYmYXwWRnJ/84MGuRm2PQBEwcR/Ek17FI3sAO04SYcuKIYkwRWRYuMdKy07I3VcGO\
 PFpZ0YFTR/4XBjm6ZpGN49iwM1BEvQ==\
 "
-ANTHROPIC_API_KEY=$(echo $ANTHROPIC_API_KEY_ENC | base64 -d | gpg -d)
+
+ZAI_API_KEY_ENC="\
+hF4DGNwPncbTkogSAQdAGgNvJbgbQG0KKEAiyjbdJNmTuk979zDSCbn65pPfcAIwD2pLtTnyUmBB\
+E1s+9/Zd1eqZQuUSDiuQAZlHjTwHhulMGeF1EVMTadqoW0c2utGp0l8BpVhLTdbg60STMA9vAt59\
+nRg903PMQ7NcFxlRPi6QXms51o36C5dVtodx5IhxAnLj3DYe3pcpl+vbjNpWCfpd4CPFkoDgJZrW\
+hUQUCcc6SiLmkskFGTbtZjmEWG44Yg==\
+"
 
 podman build -f ~/.images/opencode.Dockerfile -t localhost/opencode:latest
 podman run --rm -it \
     --network host \
     -w "$PWD" \
     -v "$PWD:$PWD:rw" \
-    -v "$HOME/.config/opencode/opencode.json:/home/node/.config/opencode/opencode.json:rw" \
+    -v "$HOME/.config/opencode/opencode.json:/root/.config/opencode/opencode.json:rw" \
     -e "ANTHROPIC_BASE_URL=https://api.proxyapi.ru/anthropic/v1" \
-    -e "ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY" \
-    -e "ZAI_BASE_URL=" \
-    -e "ZAI_API_KEY=" \
+    -e "ANTHROPIC_API_KEY=$(echo $ANTHROPIC_API_KEY_ENC | base64 -d | gpg -d)" \
+    -e "ZAI_BASE_URL=https://naomi.nau.im/api" \
+    -e "ZAI_API_KEY=$(echo $ZAI_API_KEY_ENC | base64 -d | gpg -d)" \
     localhost/opencode:latest
