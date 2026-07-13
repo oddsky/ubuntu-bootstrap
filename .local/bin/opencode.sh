08a858ff1c3212dbc4a91e076c104d82ee5b1044
Roman Rossamakhin <rossamakhin01@gmail.com>
2026-07-13 23:50:53 +0500

    fix opencode providers conf

--- .local/bin/opencode.sh
+++ .local/bin/opencode.sh
@@ -3,7 +3,7 @@
 # read KEY
 # echo $KEY | gpg --encrypt --recipient <email> --output - | base64
 
-ANTHROPIC_API_KEY_ENC="\
+PROXYAPI_API_KEY_ENC="\
 hF4DGNwPncbTkogSAQdA2JRerCuRCXWvHDMxiGPeFpho8k7pHYjTdigEvz3J90wwvYxoPAZmDSxo\
 0jfe+OBlHkzwRXbGvT5mD3mX9SQZ8RMSjJ1TSDpzhyEGL5r1O8yz0l8BRleKLtG5YctOSV0E+0pt\
 7MF5y1d1B/fPYmYXwWRnJ/84MGuRm2PQBEwcR/Ek17FI3sAO04SYcuKIYkwRWRYuMdKy07I3VcGO\
@@ -23,8 +23,6 @@ podman run --rm -it \
     -w "$PWD" \
     -v "$PWD:$PWD:rw" \
     -v "$HOME/.config/opencode/opencode.json:/root/.config/opencode/opencode.json:rw" \
-    -e "ANTHROPIC_BASE_URL=https://api.proxyapi.ru/anthropic/v1" \
-    -e "ANTHROPIC_API_KEY=$(echo $ANTHROPIC_API_KEY_ENC | base64 -d | gpg -d)" \
-    -e "ZAI_BASE_URL=https://naomi.nau.im/api" \
+    -e "PROXYAPI_API_KEY=$(echo $PROXYAPI_API_KEY_ENC | base64 -d | gpg -d)" \
     -e "ZAI_API_KEY=$(echo $ZAI_API_KEY_ENC | base64 -d | gpg -d)" \
     localhost/opencode:latest
